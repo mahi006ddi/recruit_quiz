900ba23e7c04838a5ab9d14d6e789ebf0fde2bc3
mahi006ddi <mahiro5mahiro@gmail.com>
2026-04-21 12:17:26 +0900

    問題の数値をランダムに選ぶ、答えに端数が出ないように。

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<random>
 using namespace std;
 
 //問題文と答えをまとめる構造体
@@ -11,11 +12,34 @@ struct Question
 
 int main()
 {
-	Question questions[] = {
-		{"13×-5",13 * -5},
-		{"(-21)÷(-3)",-21 / -3},
-		{"7-(4÷2)+2",7 - (4 + 2) / 2},
-	};
+	//Question questions[] = {
+	//	{"13×-5",13 * -5},
+	//	{"(-21)÷(-3)",-21 / -3},
+	//	{"7-(4÷2)+2",7 - (4 + 2) / 2},
+	//};
+	Question questions[3];
+
+	random_device rd;
+	mt19937 rand(rd());
+
+	int x = uniform_int_distribution<>(1, 30)(rand);
+	int y = uniform_int_distribution<>(1, 20)(rand);
+
+	questions[0].q = to_string(x) + "×" + to_string(y);
+	questions[0].a = x * y;
+
+	x = uniform_int_distribution<>(1, 30)(rand);
+	y = uniform_int_distribution<>(1, 20)(rand);
+	questions[1].q = to_string(x*y) + "÷" + to_string(y);
+	questions[1].a = x;
+
+
+	x = uniform_int_distribution<>(1, 100)(rand);
+	y = uniform_int_distribution<>(1, 10)(rand);
+	int z = uniform_int_distribution<>(1, 10)(rand);
+	int w = uniform_int_distribution<>(1, 10)(rand);
+	questions[2].q = to_string(x) + "ー(" + to_string(y*w) + "＋" + to_string(z*w) + ")÷" + to_string(w);
+	questions[2].a = x - (y + z);
 
 	cout << "[リクルート試験対策クイズ]\n";
 	//cout << "13×(-5)の答えは？\n";
