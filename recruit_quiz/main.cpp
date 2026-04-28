56c99d7bbaf7563b63c562ffc7fb5f6ace30bf91
mahi006ddi <mahiro5mahiro@gmail.com>
2026-04-28 10:53:27 +0900

    三角形の面積の問題追加

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 #include<string>
+#include<vector>
 #include<random>
 using namespace std;
 
 //問題文と答えをまとめる構造体
 struct Question
 {
-	string q;
-	int a;
+	string q; //問題文
+	int a;	  //答え
 };
 
 int main()
@@ -17,7 +18,7 @@ int main()
 	//	{"(-21)÷(-3)",-21 / -3},
 	//	{"7-(4÷2)+2",7 - (4 + 2) / 2},
 	//};
-	Question questions[3];
+	vector<Question> questions(3);
 
 	random_device rd;
 	mt19937 rand(rd());
@@ -40,6 +41,12 @@ int main()
 	int w = uniform_int_distribution<>(1, 10)(rand);
 	questions[2].q = to_string(x) + "ー(" + to_string(y*w) + "＋" + to_string(z*w) + ")÷" + to_string(w);
 	questions[2].a = x - (y + z);
+
+	//三角形の面積
+	x = uniform_int_distribution<>(1, 10)(rand);
+	y = uniform_int_distribution<>(1, 5)(rand) * 2;
+	questions.push_back({
+		"面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。",x });
 
 	cout << "[リクルート試験対策クイズ]\n";
 	//cout << "13×(-5)の答えは？\n";
